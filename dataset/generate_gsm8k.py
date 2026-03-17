f94ff692ba3883742e515473d297c608d3316be1
Boyi Liu <byliu0325@gmail.com>
2026-03-17 14:21:47 +0000

    (fix) update the function of dataset utils

--- dataset/generate_gsm8k.py
+++ dataset/generate_gsm8k.py
@@ -1,15 +1,16 @@
 import json
 import yaml
 
-from utils import split_dataset
+from utils import split_uniform
 
 if __name__ == "__main__":
     with open('config.yaml', 'r') as f:
         config = yaml.load(f.read(), Loader=yaml.Loader)
 
-    with open("gsm8k/train.jsonl", "r", encoding="utf-8") as f:
+    with open("gsm8k_raw/train.jsonl", "r", encoding="utf-8") as f:
         train_data = [json.loads(line) for line in f]
-    with open("gsm8k/test.jsonl", "r", encoding="utf-8") as f:
+    with open("gsm8k_raw/test.jsonl", "r", encoding="utf-8") as f:
         test_data = [json.loads(line) for line in f]
 
-    split_dataset(config, {'train': train_data, 'test': test_data})
+    all_data = train_data + test_data
+    split_uniform(all_data, config)
